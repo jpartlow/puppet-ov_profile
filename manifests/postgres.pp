9e9462022676593a91b4df99d87e1b871edab9cb
Josh Partlow <jpartlow@glatisant.org>
2026-08-13 18:31:11 -0700

    Manage postgres ssl configuration for additional ovdb servers
    The puppet-openvoxdb module currently assumes a single openvoxdb server.
    If you set openvoxdb::database::postgresql::postgresql_ssl_on to true,
    it expects a single host string for
    openvoxdb::database::postgresql::puppetdb_server.
    
    Since ovox allows for multiple ovdb instances, the profile needs to
    handle the ssl configuration for the additional servers, which it does
    here just by declaring additional instance of the
    openvoxdb::postgresql::ssl_rules defined type.
    
    Signed-off-by: Josh Partlow <jpartlow@glatisant.org>

--- manifests/postgres.pp
+++ manifests/postgres.pp
@@ -1,3 +1,21 @@
-class ov_profile::postgres {
+class ov_profile::postgres (
+  Array[String[1]] $additional_ovdb_servers = [],
+) {
   contain 'openvoxdb::database::postgresql'
+
+  $database_name     = $openvoxdb::database::postgresql::database_name
+  $database_username =
+    $openvoxdb::database::postgresql::database_username
+  $postgres_version  =
+    $openvoxdb::database::postgresql::postgres_version
+
+  $additional_ovdb_servers.each() |$ovdb| {
+    openvoxdb::database::postgresql_ssl_rules { "ovox: additional postgresql ssl rules for ${ovdb}":
+      database_name     => $database_name,
+      database_username => $database_username,
+      postgres_version  => $postgres_version,
+      puppetdb_server   => $ovdb,
+      require           => Class['openvoxdb::database::postgresql'],
+    }
+  }
 }
--- spec/classes/postgres_spec.rb
+++ spec/classes/postgres_spec.rb
@@ -8,6 +8,26 @@ describe 'ov_profile::postgres' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
+
+      context 'with multiple ovdb servers' do
+        let(:params) do
+          {
+            'additional_ovdb_servers' => ['ovdb2.spec'],
+          }
+        end
+
+        it 'generates additional ssl configuration' do
+          is_expected.to(
+            contain_openvoxdb__database__postgresql_ssl_rules('ovox: additional postgresql ssl rules for ovdb2.spec')
+              .with(
+                database_name:     'puppetdb',
+                database_username: 'puppetdb',
+                postgres_version:  '14',
+              )
+              .that_requires('Class[openvoxdb::database::postgresql]'),
+          )
+        end
+      end
     end
   end
 end
